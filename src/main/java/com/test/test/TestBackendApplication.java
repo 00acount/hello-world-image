c9ceb0652b69b99b5ba9bbe449fdc341f9060d1a
00account <00accont@gmail.com>
2023-10-04 02:44:12 +0100

    commit

---
+++ src/main/java/com/test/test/TestBackendApplication.java
@@ -0,0 +1,24 @@
+package com.test.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@CrossOrigin(origins = "*")
+@RestController
+public class TestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBackendApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> helloWorld() {
+		return ResponseEntity.ok("Hello World!");
+	}
+	
+}
